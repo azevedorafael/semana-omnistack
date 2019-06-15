12c707289a8dc9f7b2a5ae108f631d9786248274
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-06-14 22:05:45 -0300

    Add routes , components, pages and assets

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header id="main-header">
+      <div className="header-content">
+        <img src="" alt="InstaFake" />
+        <img src="" alt="Enviar publicação" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/New.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class New extends Component {
+  render() {
+    return (
+      <div>
+        <h1>New</h1>
+      </div>
+    );
+  }
+}
+
+New.propTypes = {};
+
+export default New;
