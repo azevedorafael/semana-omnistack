3643a4edc7830a47ef5fd2bddf312e9763f8e65c
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-06-14 22:19:32 -0300

    Add Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Header.css";
+
+import logo from "../assets/logo.svg";
+import camera from "../assets/camera.svg";
 
 const Header = () => {
   return (
     <header id="main-header">
       <div className="header-content">
-        <img src="" alt="InstaFake" />
-        <img src="" alt="Enviar publicação" />
+        <Link to="/">
+          <img src={logo} alt="InstaFake" />
+        </Link>
+        <Link to="/new">
+          <img src={camera} alt="Enviar publicação" />
+        </Link>
       </div>
     </header>
   );
